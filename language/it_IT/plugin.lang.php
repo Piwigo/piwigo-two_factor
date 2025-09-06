7e792ef532a582134f11ca2c68725507bf5bc8e1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-09-06 18:37:04 +0200

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Inactive'] = 'Inattivo';
+$lang['Invalid recovery code'] = 'Codice di recupero errato';
+$lang['Scan the QR code'] = 'Scansiona il codice QR';
+$lang['Select an authentication method.'] = 'Seleziona un metodo d\'autenticazione';
+$lang['Send again.'] = 'Invia di nuovo';
+$lang['Send email'] = 'Invia email';
+$lang['Emails don\'t match'] = 'Le email non corrispondono';
+$lang['Enable 2FA by application'] = 'Abilita 2FA per applicazione';
+$lang['Enable 2FA by email'] = 'Abilita 2FA per email';
+$lang['Enter one of your recovery codes'] = 'Inserisci uno dei tuoi codici di recupero.';
+$lang['Enter the code'] = 'Inserisci il codice';
+$lang['Error when sending email'] = 'Errore nell\'invio del messaggio di posta elettronica';
+$lang['2FA by application'] = '2FA per applicazione';
+$lang['2FA by email'] = '2FA per email';
+$lang['Active'] = 'Attiva';
+$lang['Auth app'] = 'Le applicazioni di autenticazione e le estensioni del browser come 1Password, Authy, Microsoft Authenticator, ecc. generano delle password monouso, le quali vengono utilizzate come secondo fattore per verificare la tua identità qualora richiesto durante l\'accesso.';
+$lang['Authentication app'] = 'App per l\'autenticazione';
+$lang['Can\'t connect? <u>Use a recovery code.</u>'] = 'Non puoi connetterti? <u>Utilizza un codice di ripristino.</u>';
+$lang['Choose another authentication method'] = 'Seleziona un altro metodo d\'autenticazione';
+$lang['Configure your two-factor authentication settings and secure your acount'] = 'Configura le impostazioni per il tuo metodo d\'autenticazione a due fattori e metti il tuo account in sicurezza';
+$lang['Confirm your email'] = 'Conferma la tua email';
+$lang['Deactivated for next connection'] = 'Disattiva per la prossima connessione';
+$lang['Disable 2FA authentication'] = 'Disattiva l\'autenticazione a due fattori';
+$lang['Disable 2FA authentication for next connection'] = 'Disattiva l\'autenticazione a due fattori per la prossima connessione';
+$lang['Disable in plugin configuration'] = 'Disabilita all\'interno della configurazione del plugin';
