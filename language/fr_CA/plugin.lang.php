eea17c2cdeaa2c1b0f03c505f18fbc06596264b3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-09-06 18:36:59 +0200

    Update fr_CA, thanks to bgoutier

---
+++ language/fr_CA/plugin.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Use an authentication application or browser extension to scan it. <a>Find out more about activating 2FA.</a>'] = 'Utilisez une application d\'authentification ou une extension de navigateur pour le scanner. <a>En savoir plus sur l\'activation de 2FA.</a>';
+$lang['Unable to scan ? Get the <u>setup key</u>'] = 'Impossible de scanner ? Obtenir <u>la clé de configuration</u>';
+$lang['Enter the verification code sent to your email address'] = 'Saisissez le code de vérification envoyé à votre courriel';
+$lang['Error when sending email'] = 'Erreur lors de l\'envoi du courriel';
+$lang['Please add an email first before activating two factor authentication by email.'] = 'Veuillez d\'abord ajouter un courriel avant de vouloir activer l\'authentification à deux facteurs par courriel.';
+$lang['Please wait %s seconds before sending an email again.'] = 'Veuillez attendre %s secondes avant d\'envoyer un courriel à nouveau.';
+$lang['Send email'] = 'Envoyer le courriel';
+$lang['Use your email to authenticate'] = 'Utilisez votre courriel pour vous authentifier';
+$lang['Validate with app'] = 'Valider avec l\'application';
+$lang['Validate with email'] = 'Valider avec le courriel';
+$lang['We will send you the authentication code by email. This method is not the most secure.'] = 'Nous vous enverrons le code d\'authentification par courriel. Cette méthode n\'est pas la plus sûre.';
+$lang['You didn’t get the Email?'] = 'Vous n\'avez pas reçu le courriel ?';
+$lang['Your Email'] = 'Votre courriel';
+$lang['Set up using Email'] = 'Configuration par courriel';
+$lang['The email address is missing'] = 'L\'adresse courriel est manquante';
+$lang['2FA by email'] = '2FA par courriel';
+$lang['Confirm your email'] = 'Confirmez votre courriel';
+$lang['Do you really want to disable two factor authentication by email?'] = 'Voulez-vous vraiment désactiver l\'authentification à deux facteurs par courriel ?';
+$lang['Email two-factor authentication has been successfully enabled'] = 'L\'authentification à deux facteurs par courriel a été activée avec succès.';
+$lang['Emails don\'t match'] = 'Les courriels ne correspondent pas';
+$lang['Enable 2FA by email'] = 'Activer 2FA par courriel';
