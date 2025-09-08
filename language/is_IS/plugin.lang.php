b46eb5a17d935790f2bb5812663dbd0c1f726f3a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-09-08 05:50:08 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['2FA by application'] = '2FA með forriti';
+$lang['2FA by email'] = '2FA með tölvupósti';
+$lang['Active'] = 'Virkt';
+$lang['Auth app'] = 'Auðkenningarforrit og vafraviðbætur eins og 1Password, Authy, Microsoft Authenticator o.s.frv. búa til einnota lykilorð sem eru notuð sem annar þáttur til að staðfesta auðkenni þitt þegar beðið er um það við innskráningu.';
+$lang['Authentication app'] = 'Auðkenningarforrit';
+$lang['Can\'t connect? <u>Use a recovery code.</u>'] = 'Geturðu ekki tengst? <u>Notaðu endurheimtunarkóða.</u>';
+$lang['Choose another authentication method'] = 'Veldu aðra auðkenningaraðferð';
+$lang['Configure your two-factor authentication settings and secure your acount'] = 'Stilltu tvíþátta auðkenningu og tryggðu öryggi reikningsins';
+$lang['Confirm your email'] = 'Staðfestu þitt netfangið';
+$lang['Deactivated for next connection'] = 'Óvirkt fyrir næstu tengingu';
+$lang['Disable 2FA authentication'] = 'Slökkva á 2FA auðkenningu';
+$lang['Disable 2FA authentication for next connection'] = 'Slökkva á 2FA auðkenningu fyrir næstu tengingu';
+$lang['Disable in plugin configuration'] = 'Slökkva í stillingum viðbótar';
+$lang['Do you really want to disable two factor authentication by application?'] = 'Viltu virkilega slökkva á tveggja þátta auðkenningu með forriti?';
+$lang['Do you really want to disable two factor authentication by email?'] = 'Viltu virkilega slökkva á tvíþátta auðkenningu með tölvupósti?';
+$lang['Email two-factor authentication has been successfully enabled'] = 'Tvíþátta auðkenning tölvupósts hefur verið virkjuð';
+$lang['Emails don\'t match'] = 'Netföngin ekki samstæð';
+$lang['Enable 2FA by application'] = 'Virkja 2FA með forriti';
+$lang['Enable 2FA by email'] = 'Virkja 2FA með tölvupósti';
+$lang['Enter one of your recovery codes'] = 'Sláðu inn einn af endurheimtarkóðunum þínum';
