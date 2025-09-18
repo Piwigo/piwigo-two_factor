e4eaeefd995d415eea105ce0108c25ec0e5b36a7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-09-18 13:00:18 +0200

    Update nl_NL, thanks to navisence

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['2FA by application'] = '2FA via app';
+$lang['2FA by email'] = '2FA via email';
+$lang['Active'] = 'Actief';
+$lang['Authentication app'] = 'Authenticatie-app';
+$lang['Choose another authentication method'] = 'Kies een andere authenticatiemethode';
+$lang['Confirm your email'] = 'Bevestig uw email';
