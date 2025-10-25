104c5e139323017ee065c28a0a7427367a20694f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-10-25 20:25:34 +0200

    Update nl_NL, thanks to pauldaytona

--- language/nl_NL/plugin.lang.php
+++ language/nl_NL/plugin.lang.php
@@ -16,3 +16,4 @@ $lang['Error when sending email'] = 'Fout bij het versturen van email';
 $lang['If you are unable to connect, please contact the webmaster'] = 'Als je niet kan verbinden, neem contact op met de webmaster';
 $lang['If you did not request this setup, please contact your administrator immediately.'] = 'Als je niet om deze setup hebt gevraagd, neem direct op met de beheerder.';
 $lang['Auth app'] = 'Authenticatietoepassingen en browserextensies zoals 1Password, Authy, Microsoft Authenticator, etc. genereren eenmalige wachtwoorden die als tweede factor worden gebruikt om uw identiteit te verifiëren wanneer hierom wordt gevraagd tijdens het inloggen.';
+$lang['Maximum number of failed attempts before lockout'] = 'Maximum aantal foute pogingen voor blokkering inloggen';
