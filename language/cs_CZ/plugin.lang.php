25e44428bbc3a027fdd96e83aa9be189473709d0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-01-29 05:51:48 +0100

    Update cs_CZ, thanks to webprostor

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,21 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Use an authentication application or browser extension to scan it. <a>Find out more about activating 2FA.</a>'] = 'Pro skenování použijte autentizační aplikaci nebo rozšíření prohlížeče. <a>Zjistěte více o aktivaci 2FA.</a>';
+$lang['Use an external authentication application to obtain authentication codes.'] = 'Použijte externí aplikaci pro 2FA ověřování k získání ověřovacích kódů.';
+$lang['Use your email to authenticate'] = 'Použijte váš e-mail pro ověření.';
+$lang['Use your external app to authenticate'] = 'Použijte vaši externí aplikaci pro 2FA ověřování';
+$lang['Validate with app'] = 'Ověřit přez aplikaci';
+$lang['Validate with email'] = 'Ověřit přez e-mail';
+$lang['Verify'] = 'Ověřit';
+$lang['We have prepared a documentation page to help you, with screenshots.'] = 'Připravili jsme pro vás stránku dokumentace se screenshoty.';
+$lang['We will send you the authentication code by email. This method is not the most secure.'] = 'Odešleme vám ověřovací kód na e-mail. Tato metoda není tak bezpečná, proto ji spíše nedoporučujeme.';
+$lang['You are setting up two-factor authentication for your account.'] = 'Nastavil/a jste dvou-faktorové ověření k přihlášení se pro váš účet.';
+$lang['You didn’t get the Email?'] = 'Neobdržel jste e-mail?';
+$lang['Your Email'] = 'Váš e-mail';
+$lang['Your verification code is: %s'] = 'Váš ověřovací kód je: %s';
+$lang['two_factor_js'] = 'Dvoufaktorové ověřování';
